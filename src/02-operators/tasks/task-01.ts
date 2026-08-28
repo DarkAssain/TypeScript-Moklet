2f6f9b7d7091cdde7e59326ad68869b2e3b52934
DarkAssain <nabeelmontesky@gmail.com>
2026-08-28 17:13:52 +0700

    completed operators section

--- src/02-operators/tasks/task-01.ts
+++ src/02-operators/tasks/task-01.ts
@@ -4,7 +4,7 @@
  *  - Total price of fried rice
  *  - Total price of drinks
  *  - Total price before discount
- *  - Final amount to be paid
+ *  - Final amount to be paid 
  *
  * Task:
  * 1. Use operators to calculate:
@@ -14,3 +14,21 @@
  *  - Final payment
  * 2. Display the calculation results.
  */
+
+const friedRicePrice: number = 18000;
+const friedRiceQuantity: number = 3;
+
+const waterPrice: number = 5000;
+const waterQuantity: number = 2;
+
+const discount: number = 10000;
+
+const totalFoodPrice: number = friedRicePrice * friedRiceQuantity;
+const totalDrinkPrice: number = waterPrice * waterQuantity;
+const grandTotal: number = totalFoodPrice + totalDrinkPrice;
+const finalPayment: number = grandTotal - discount;
+
+console.log(`Total price of fried rice:   Rp${totalFoodPrice.toLocaleString('id-ID')}`);
+console.log(`Total price of drinks:       Rp${totalDrinkPrice.toLocaleString('id-ID')}`);
+console.log(`Total price before discount: Rp${grandTotal.toLocaleString('id-ID')}`);
+console.log(`Final amount to be paid:     Rp${finalPayment.toLocaleString('id-ID')}`);
